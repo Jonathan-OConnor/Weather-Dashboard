8da12023207af108c800d96760e3721af2d38f2a
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-01-28 18:45:51 -0500

    updated api call to https instead of http

--- script.js
+++ script.js
@@ -19,17 +19,19 @@ function buildFavs() {
         favRow.appendChild(newFav)
     }
 }
+
 async function citySearch(event) {
     event.preventDefault()
 
-    // make API call using city name entered in search bar
+    // make API call using city name entered in search bar to get current weather data
     city = document.getElementById("city").value
-    currentData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=557ace5a3feab4dc1227705af9d390c9`).then(r => r.json())
+    currentData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=557ace5a3feab4dc1227705af9d390c9`).then(r => r.json())
 
     lat= currentData.coord.lat
     lon= currentData.coord.lon
 
-    currentUVData = await fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=557ace5a3feab4dc1227705af9d390c9`).then(r => r.json())
+    // make API to get the city's current UV data
+    currentUVData = await fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=557ace5a3feab4dc1227705af9d390c9`).then(r => r.json())
     
     // get important data from returned object
     var currentTemp = Math.round(currentData.main.temp)
