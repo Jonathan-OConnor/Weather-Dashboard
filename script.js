7afeb2ff4a42356d5242a564211951e7a5503332
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-01-29 13:29:06 -0500

    added functionality to favourite buttons

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ if (localStorage.favs) {
 }
 
 document.getElementById("date").innerText = currentDayFormat
+
 buildFavs()
 buildForeCast()
 
@@ -18,12 +19,13 @@ function buildFavs() {
         document.getElementById("favourites").appendChild(favRow)
         var newFav = document.createElement("button")
         newFav.setAttribute("class", "btn btn-outline-dark btn-test")
+        newFav.setAttribute("onClick", `updateInformation("${favs[i]}")`)
         newFav.innerText = favs[i]
         favRow.appendChild(newFav)
     }
 }
 
-function buildForeCast(){
+function buildForeCast() {
     for (var i = 1; i < 6; i++) {
         document.getElementById(`day-${i}-date`).innerText = currentDay.add(1, 'd').format("M/D/Y")
     }
@@ -35,19 +37,24 @@ async function citySearch(event) {
 
     // make API call using city name entered in search bar to get current weather data
     city = document.getElementById("city").value
+    updateInformation(city)
+
+}
+
+async function updateInformation(city) {
     currentData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=557ace5a3feab4dc1227705af9d390c9`).then(r => r.json())
 
     lat = currentData.coord.lat
     lon = currentData.coord.lon
 
     // make API to get the city's current UV data
-    currentUVData = await fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=557ace5a3feab4dc1227705af9d390c9`).then(r => r.json())
+    forecastData = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=metric&appid=557ace5a3feab4dc1227705af9d390c9`).then(r => r.json())
 
     // get important data from returned object
     var currentTemp = Math.round(currentData.main.temp)
     var currentHumidity = currentData.main.humidity
     var currentWindSpeed = Math.round(currentData.wind.speed * 36) / 10
-    var currentUVIndex = currentUVData.value
+    var currentUVIndex = forecastData.current.uvi
 
 
     // update text area to display weather data
@@ -57,18 +64,17 @@ async function citySearch(event) {
     document.getElementById("wind-speed").innerText = `Wind Speed: ${currentWindSpeed} KPH`
     document.getElementById("uv-index").innerText = `UV Index: ${currentUVIndex}`
 
+
     // get information for 5 day forecast
-    forecastData = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=557ace5a3feab4dc1227705af9d390c9`).then(r => r.json())
+    for (var i = 1; i < 6; i++) {
+        var temp = Math.round(forecastData.daily[i].temp.day)
+        var humidity = forecastData.daily[i].humidity
 
-    for (var i = 0; i < forecastData.list.length; i += 8) {
-        var temp = Math.round(forecastData.list[i].main.temp)
-        var humidity = forecastData.list[i].main.humidity
-
-        document.getElementById(`day-${(i + 8) / 8}-temp`).innerText = `Temperature: ${temp} °C`
-        document.getElementById(`day-${(i + 8) / 8}-humidity`).innerText = `Humidity: ${humidity}%`
+        document.getElementById(`day-${i}-temp`).innerText = `Temperature: ${temp} °C`
+        document.getElementById(`day-${i}-humidity`).innerText = `Humidity: ${humidity}%`
     }
-    
-    console.log(forecastData)
+
+
     // add onto the favourites column if city is not already in favourites
     if (favs.includes(currentData.name) == false) {
         var favRow = document.createElement("div")
@@ -76,14 +82,11 @@ async function citySearch(event) {
         document.getElementById("favourites").appendChild(favRow)
         var newFav = document.createElement("button")
         newFav.setAttribute("class", "btn btn-outline-dark btn-test")
+        newFav.setAttribute("onClick", `updateInformation("${currentData.name}")`)
         newFav.innerText = currentData.name
         favRow.appendChild(newFav)
 
         favs.push(currentData.name)
         localStorage.favs = JSON.stringify(favs)
     }
-
-
-
 }
-
